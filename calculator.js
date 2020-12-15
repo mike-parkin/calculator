da1026505186e80296f25c28d8d1eb0118c26938
Mike Parkin <michael.parkin1988@gmail.com>
2020-12-15 15:01:03 +1300

    add html and js psuedocode

---
+++ calculator.js
@@ -0,0 +1,13 @@
+// create vairable to store current entry in
+// create an array to push the save the entries in
+// create a varible for the total
+// create click events for buttons adding the button text into another variable
+
+
+// if button entry is a number or decimal point it is stored as a string in the entry variable
+// if the button 'AC' button is clicked then all varibles are emptied
+// if the button 'CE' is clicked then the varible for the current entry becomes ''
+// if a mathmatical operator is clicked (x,+,/,-) then the current string is pushed into the array, then the operator is added after
+// when the equals is clicked, loop through the array parsing the first element and every second element from there on in to a number varaible
+// the loop also checks the operators and perfoms the relative operation for the two numbers on either side of it on the array.
+// the answer is then displaed on the screen
