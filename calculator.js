8f22b7ae3582b4f7b053bcd924850a0a1c1b9063
Mike Parkin <michael.parkin1988@gmail.com>
2020-12-17 14:09:05 +1300

    add clickEvents for buttons

--- calculator.js
+++ calculator.js
@@ -13,3 +13,35 @@
 // the answer is then displaed on the screen
 
 console.log("Hello, this is my calculator.js file");
+
+var temp = '';
+var entry = [];
+var total = 0;
+
+document.getElementById('AC').addEventListener('click', function() {clearEntries('AC')})
+document.getElementById('CE').addEventListener('click', function() {clearEntries('CE')})
+document.getElementById('0').addEventListener('click', function() {addEntry('0')});
+document.getElementById('1').addEventListener('click', function() {addEntry('1')});
+document.getElementById('2').addEventListener('click', function() {addEntry('2')});
+document.getElementById('3').addEventListener('click', function() {addEntry('3')});
+document.getElementById('4').addEventListener('click', function() {addEntry('4')});
+document.getElementById('5').addEventListener('click', function() {addEntry('5')});
+document.getElementById('6').addEventListener('click', function() {addEntry('6')});
+document.getElementById('7').addEventListener('click', function() {addEntry('7')});
+document.getElementById('8').addEventListener('click', function() {addEntry('8')});
+document.getElementById('9').addEventListener('click', function() {addEntry('9')});
+document.getElementById('decimal').addEventListener('click', function() {addEntry('.')});
+
+function addEntry(str) {
+    temp += str;
+    console.log(temp);
+}
+
+function clearEntries(str) {
+    if (str === 'AC') {
+        temp = ''
+        entry = []
+    } else if (str === 'CE') {
+        temp = ''
+    }
+}
